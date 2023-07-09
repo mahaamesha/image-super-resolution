0431f4a00ba2e2d290313444038db69e5012db1d
mahaamesha <mahaamesha@gmail.com>
2023-07-09 15:04:44 +0700

    add ssim func

--- src/eval_func.py
+++ src/eval_func.py
@@ -36,3 +36,26 @@ def calculate_psnr(im1, im2):
 #     features2 = vgg_model.predict(im2)
 #     vgg_loss = np.mean((features1 - features2) ** 2)
 #     return vgg_loss
+
+
+def get_ssim(im1, im2, k1=0.01, k2=0.03, L=255):
+    """ compute Structural Similarity Index (SSIM) between two images.\\
+        1 indicating a perfect match and -1 indicating a complete mismatch.\\
+
+        im1  : Any   deblured images using wiener filter with certain psf\\
+        im2  : Any   original image without blur\\
+        k1, k2  : float constants used for stability control.\\
+        L   : int   max value of pixel
+    """
+    c1 = (k1*L)**2
+    c2 = (k2*L)**2
+    im1 = im1.astype(np.float64)
+    im2 = im2.astype(np.float64)
+    mean1 = np.mean(im1)
+    mean2 = np.mean(im2)
+    var1 = np.var(im1)
+    var2 = np.var(im2)
+    cov12 = np.cov(im1.flat, im2.flat)[0,1]
+    numerator = (2*mean1*mean2 + c1) * (2*cov12 + c2)
+    denominator = (mean1**2 + mean2**2 + c1) * (var1 + var2 + c2)
+    return numerator / denominator
